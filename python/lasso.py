aca9a4e7604a0606ecb5be8331eebf4fdcec8b58
ssoi <sameer.soi@grandrounds.com>
2018-12-16 12:55:02 -0800

    Update batching

--- python/lasso.py
+++ python/lasso.py
@@ -9,15 +9,20 @@ class TFLasso(BaseEstimator, ClassifierMixin):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.batch_size = batch_size
+        self.coef_ = None
 
+        self._X_tensor = None
+        self._y_tensor = None
+        self._beta = None
+        self._beta_0 = None
         self._tf_model = None
 
     def fit(self, X, y):
         if self._tf_model is None:
-            self._build_tf_model(X, y)
+            self._build_tf_model(self.batch_size, X.shape[1])
 
         
-        optimizer = tf.GradientDescentOptimizer(learning_rate=self.learning_rate).\
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate).\
             minimize(self._tf_model)
 
         init = tf.global_variables_initializer()
@@ -28,33 +33,35 @@ class TFLasso(BaseEstimator, ClassifierMixin):
             num_batches = int(X.shape[0]/self.batch_size)
             for epoch in range(self.epochs):
                 for _ in range(num_batches):
-                    X_batch, y_batch = _get_batch(X, y, self.batch_size)
-                    sess.run([optimizer, self._tf_model], feed_dict={X: X_batch, y: y_batch})
+                    X_batch, y_batch = self._get_batch(X, y)
+                    sess.run(
+                        [optimizer, self._tf_model],
+                        feed_dict={self._X_tensor: X_batch, self._y_tensor: y_batch}
+                    )
+
+            self.coef_ = (sess.run(self._beta), sess.run(self._beta_0))
 
     def predict(self):
         pass
 
-    def _build_tf_model(self, X, y):
-        if X.shape[0] != y.shape[0]:
-            raise ValueError('Number of observations must match between X and y')
-
-        num_obs, num_features = X.shape
-
-        X_tensor = tf.placeholder(tf.float32, [self.batch_size, num_features], name='X')
-        y_tensor = tf.placeholder(tf.float32, [self.batch_size, 1], name='y')
+    def _build_tf_model(self, num_obs, num_features):
+        self._X_tensor = tf.placeholder(tf.float32, [num_obs, num_features], name='X')
+        self._y_tensor = tf.placeholder(tf.float32, [num_obs, 1], name='y')
         
-        beta = tf.Variable(tf.random_normal(shape=[num_features, 1], stddev=0.1), name='coefs')        
-        beta_0 = tf.Variable(tf.random_normal(shape=[1, 1], stddev=0.1), name='intercept')
+        self._beta = tf.Variable(tf.random_normal(shape=[num_features, 1], stddev=0.1), name='coefs')        
+        self._beta_0 = tf.Variable(tf.random_normal(shape=[1, 1], stddev=0.1), name='intercept')
 
-        logits = tf.matmul(X_tensor, beta) + beta_0
-        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
-        loss = tf.reduce_mean(cross_entropy) + alpha * tf.norm(beta)
+        logits = tf.matmul(self._X_tensor, self._beta) + self._beta_0
+        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=self._y_tensor, logits=logits)
+        loss = tf.reduce_mean(cross_entropy) + self.alpha * tf.abs(self._beta)
 
         self._tf_model = loss
 
-    @staticmethod
-    def _get_batch(X, y, batch_size):
+    def _get_batch(self, X, y):
         num_obs = X.shape[0]
-        idx = np.random.choice(num_obs, batch_size)
+        idx = np.random.choice(num_obs, self.batch_size)
 
-        return X[idx, :], y[idx]
+        y_batch = y[idx].reshape(self.batch_size, 1)
+        X_batch = X[idx, :]
+
+        return X_batch, y_batch
